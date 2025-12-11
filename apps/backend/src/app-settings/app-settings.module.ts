d8b37e00421987660cadfe35ea4fbbc1572916ef
Robin Edquist <robin.edquist@helo.se>
2025-12-11 01:05:26 +0100

    Fix error

--- apps/backend/src/app-settings/app-settings.module.ts
+++ apps/backend/src/app-settings/app-settings.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppSettingsService } from './app-settings.service';
 import { AppSettingsController } from './app-settings.controller';
 import { OidcConfigService } from '../auth/oidc-config.service';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   controllers: [AppSettingsController],
   providers: [AppSettingsService, OidcConfigService],
   exports: [AppSettingsService],
