2ec9dddb644d6843029791bac3eb4af19bd86b54
Robin Edquist <robin.edquist@helo.se>
2026-06-14 23:14:39 +0200

    feat(comics): collections service (CRUD, membership, list, detail)

--- apps/backend/src/comics/comics.module.ts
+++ apps/backend/src/comics/comics.module.ts
@@ -1,6 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { ComicsController } from './comics.controller';
 import { ComicsService } from './comics.service';
+import { ComicsCollectionsService } from './comics-collections.service';
 import { LibraryWatcherModule } from '../library-watcher/library-watcher.module';
 import { CanEditMetadataGuard } from '../common/guards/can-edit-metadata.guard';
 import { CanDeleteGuard } from '../common/guards/can-delete.guard';
@@ -8,7 +9,7 @@ import { CanDeleteGuard } from '../common/guards/can-delete.guard';
 @Module({
   imports: [forwardRef(() => LibraryWatcherModule)],
   controllers: [ComicsController],
-  providers: [ComicsService, CanEditMetadataGuard, CanDeleteGuard],
-  exports: [ComicsService],
+  providers: [ComicsService, ComicsCollectionsService, CanEditMetadataGuard, CanDeleteGuard],
+  exports: [ComicsService, ComicsCollectionsService],
 })
 export class ComicsModule {}
